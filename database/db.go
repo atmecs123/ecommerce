9b62aaf27bd8f34d12680d0cc31d29fdb61f1641
atmecs123 <siddhantdoshi280592@gmail.com>
2024-04-08 18:30:15 +0530

    Add retry for db connection

--- database/db.go
+++ database/db.go
@@ -1,8 +1,10 @@
 package database
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
@@ -24,15 +26,31 @@ func InitDb() (*gorm.DB, error) {
 	return DB, err
 }
 
+func retry(maxRetries int, delay time.Duration, fn func() (*gorm.DB, error)) (*gorm.DB, error) {
+	fmt.Println("###### Inside retry #####")
+	for i := 0; i < maxRetries; i++ {
+		gormDb, err := fn()
+		if err == nil {
+			fmt.Println("###### Retry count ####### ", i)
+			return gormDb, err // Success
+		}
+		fmt.Printf("Attempt %d failed; retrying in %v...\n", i+1, delay)
+		time.Sleep(delay)
+	}
+	return nil, errors.New("maximum retries reached")
+}
+
 func ConnectDB() (*gorm.DB, error) {
 	dsn := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD") + "@tcp" + "(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME") + "?" + "parseTime=true&loc=Local"
 	fmt.Println("Full database connection name", dsn)
-	gormDB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		fmt.Println("Error connecting to my sql database", err)
+	gormOpenfn := func() (*gorm.DB, error) {
+		gormDB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err != nil {
+			fmt.Println("Error connecting to my sql database", err)
+			return gormDB, err
+		}
 		return gormDB, err
 	}
-
+	gormDB, err := retry(4, 2*time.Second, gormOpenfn)
 	return gormDB, err
-
 }
